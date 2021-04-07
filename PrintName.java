2454b87b39a1f488c8ff8794c66b063f81e11022
Chaitanya S <77399053+ChaitanyaSuyog@users.noreply.github.com>
2021-04-07 09:28:15 -0700

    Update PrintName.java

--- PrintName.java
+++ PrintName.java
@@ -1,7 +1,5 @@
 //Program 1- WAP to print your name
 
-package com.chaitanya;
-
 public class PrintName {
     public static void main(String[] args) {
         System.out.println("My name is Chaitanya");
