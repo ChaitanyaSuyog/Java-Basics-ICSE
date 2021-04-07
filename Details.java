379284ecc010a6dd5c13454045d6bdc828211d43
Chaitanya S <77399053+ChaitanyaSuyog@users.noreply.github.com>
2021-04-07 09:27:58 -0700

    Update Details.java

--- Details.java
+++ Details.java
@@ -1,7 +1,5 @@
 //Program 2- WAP to display your name, class, section and school.
 
-package com.chaitanya;
-
 public class Details {
     public static void main(String[] args) {
         System.out.println("Name: Chaitanya,");
