6514137fd92a498250e0dcc51967de5bb9e02c79
Chaitanya S <77399053+ChaitanyaSuyog@users.noreply.github.com>
2021-04-07 09:28:55 -0700

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,7 +1,5 @@
 // Program 9- WAP to calculate and display the simple interest and amount given the principal, amount, rate per annum and time (in years).
 
-package com.chaitanya;
-
 import java.util.Scanner;
 
 public class SimpleInterest {
