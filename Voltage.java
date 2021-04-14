89776c5e0e6b34e3aed23242b93ddc1e5ffd56ae
Chaitanya S <chaitanya.suyog@gmail.com>
2021-04-14 14:57:32 +0530

    Program 11

---
+++ Voltage.java
@@ -0,0 +1,15 @@
+// Program 11- WAP to calculate voltage using V = IR.
+
+import java.util.Scanner;
+
+public class Voltage {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the current and resistance: ");
+        int c = sc.nextInt();
+        int r = sc.nextInt();
+        double v = c * r;
+        System.out.println("Voltage = " + v);
+    }
+}
