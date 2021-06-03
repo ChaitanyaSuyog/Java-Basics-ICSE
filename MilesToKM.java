0b2aca3982be5308e200ea5cb70b615a99766e29
Chaitanya S <chaitanya.suyog@gmail.com>
2021-06-03 10:55:42 +0530

    Program 11
    Added program 11

---
+++ MilesToKM.java
@@ -0,0 +1,16 @@
+//Program 11- WAP to convert and display the distance given in miles to kilometers.
+
+import java.util.Scanner;
+
+public class MilesToKM {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the distance in miles:");
+        double miles = scan.nextDouble();
+
+        double kilometers = 1.60934d;
+        double finalKm = miles * kilometers;
+
+        System.out.println("The distance is " + finalKm + " kilometers.");
+    }
+}
