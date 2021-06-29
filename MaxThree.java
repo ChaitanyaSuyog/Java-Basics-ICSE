f55093b6e60ef26708819014afea00bbd7af2832
Chaitanya S <chaitanya.suyog@gmail.com>
2021-06-29 11:11:08 +0530

    Program 16
    Some programs left out - will do later

---
+++ MaxThree.java
@@ -0,0 +1,16 @@
+//Program 16 - WAP to find the largest out of three given numbers.
+
+import java.util.Scanner;
+
+public class MaxThree {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter three numbers: ");
+        double num1 = scan.nextDouble();
+        double num2 = scan.nextDouble();
+        double num3 = scan.nextDouble();
+
+        double max = Math.max(Math.max(num1, num2), num3);
+        System.out.println("Maximum number = " + max);
+    }
+}
