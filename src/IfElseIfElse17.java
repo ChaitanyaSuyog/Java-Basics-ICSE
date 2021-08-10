aba075907a5a48c8b4b606c4d9770431274bb86d
Chaitanya S <77399053+ChaitanyaSuyog@users.noreply.github.com>
2021-08-10 19:34:07 +0530

    Update IfElseIfElse17.java

--- src/IfElseIfElse17.java
+++ src/IfElseIfElse17.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class IfElseIfElse17 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a no.: ");
         float a = scan.nextFloat();
 
         if(a > 0) {
