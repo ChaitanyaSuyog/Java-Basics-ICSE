83bf99f7e1921ba923b5c4932bb5570376ed3e5b
Chaitanya S <chaitanya.suyog@gmail.com>
2021-08-16 11:43:59 +0530

    Program 22

---
+++ src/EvenFor22.java
@@ -0,0 +1,11 @@
+//Program 22 - WAP to print even numbers between 20 and 30.
+
+package src;
+
+public class EvenFor22 {
+    public static void main(String[] args) {
+        for (int i = 22; i < 30; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
