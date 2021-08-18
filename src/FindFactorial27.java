49cba294adb0d27a86a90d322ad39e17ee957ec4
Chaitanya S <chaitanya.suyog@gmail.com>
2021-08-18 11:51:55 +0530

    Program 27

---
+++ src/FindFactorial27.java
@@ -0,0 +1,22 @@
+//Program 27 - WAP to find the factorial of a given number.
+
+package src;
+
+import java.util.Scanner;
+
+public class FindFactorial27 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int number = scan.nextInt();
+
+        System.out.println("The factorial of " + number + " is:");
+
+        int factorial = 1;
+
+        for (int i = 1; i <= number; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+    }
+}
