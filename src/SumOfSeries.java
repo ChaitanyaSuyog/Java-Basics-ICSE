c6eb94e4063c385801e190b5e81f2026e5c92523
Chaitanya S <chaitanya.suyog@gmail.com>
2021-08-23 12:00:03 +0530

    Program 30
    Completed Program 30 - for loop

--- src/SumOfSeries.java
+++ src/SumOfSeries.java
@@ -7,6 +7,19 @@ import java.util.Scanner;
 public class SumOfSeries {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the number of terms: ");
+        System.out.println("Enter the number of terms and the value of x: ");
+        int n = scan.nextInt();
+        int x = scan.nextInt();
+
+        double sum = 0d;
+        int j = 1;
+
+        for (int i = 1; i <= n; i++) {
+            double power = j++;
+            double eq = 1/Math.pow(x, power);
+            double t = eq;
+            sum = sum + t;
+        }
+        System.out.println(sum);
     }
 }
