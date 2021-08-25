e356a328b912676ce36ce7bc0e90ad28ed8174f5
Chaitanya S <chaitanya.suyog@gmail.com>
2021-08-25 11:56:04 +0530

    Program 32
    Created program 32 - to find the sum of a series.

---
+++ src/SumSeriesfactorial32.java
@@ -0,0 +1,27 @@
+//Program 31 - WAP to find the sum of the given numbers : x/1! - x^2/2! + x^3/3! - x^4/4!... upto n terms
+
+package src;
+
+import java.util.Scanner;
+
+public class SumSeriesfactorial32 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the value of x and the number of terms: ");
+        double x = scan.nextDouble();
+        int n = scan.nextInt();
+
+        double sum = 0f;
+        int f = 1;
+
+        for (int i = 1; i <= n; i ++) {
+            f = f * i;
+            if (i % 2 != 0) {
+                sum = sum + ((Math.pow(x, i)) / f);
+            }
+            else {
+                sum = sum - ((Math.pow(x, i)) / f);
+            }
+        }
+    }
+}
