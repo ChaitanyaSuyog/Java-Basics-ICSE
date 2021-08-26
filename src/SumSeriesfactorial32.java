3fe6e8edbd722fb7af67c9e08764f6bda7801e46
Chaitanya S <chaitanya.suyog@gmail.com>
2021-08-26 23:06:47 +0530

    Minor

--- src/SumSeriesfactorial32.java
+++ src/SumSeriesfactorial32.java
@@ -9,12 +9,12 @@ public class SumSeriesfactorial32 {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the value of x and the number of terms: ");
         double x = scan.nextDouble();
-        int n = scan.nextInt();
+        int num = scan.nextInt();
 
         double sum = 0f;
         int f = 1;
 
-        for (int i = 1; i <= n; i ++) {
+        for (int i = 1; i <= num; i ++) {
             f = f * i;
             if (i % 2 != 0) {
                 sum = sum + ((Math.pow(x, i)) / f);
