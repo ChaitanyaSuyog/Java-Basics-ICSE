8c1d4a099c66b2b738e9e6b06ec3c4d674567768
Chaitanya S <chaitanya.suyog@gmail.com>
2021-09-28 12:28:02 +0530

    Program 33

---
+++ src/While.java
@@ -0,0 +1,11 @@
+package src;
+
+public class While {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
