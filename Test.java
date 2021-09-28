a909f0ebafde03ba8d9cb88c489ae356e24b9b87
Chaitanya S <chaitanya.suyog@gmail.com>
2021-09-28 12:28:47 +0530

    .gitignore

--- Test.java
+++ Test.java
@@ -1,8 +1,22 @@
-public class Test
-{
+import java.util.Scanner;
+
+class PrimeNum {
     public static void main(String[] args) {
-        for (int i = 1, x = 1; i <= 10; x++, i++) {
-            System.out.println(i);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter an integer to check whether it is prime or not: ");
+        int number = scan.nextInt();
+        int count = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            System.out.println("The number is prime.");
+        }
+        else {
+            System.out.println("The number is not prime.");
         }
     }
 }
