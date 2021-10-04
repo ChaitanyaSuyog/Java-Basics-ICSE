ab7727acd4ba6ecaf2f16e700068537a9f605e6b
Chaitanya S <chaitanya.suyog@gmail.com>
2021-10-04 11:32:48 +0530

    Program 34

---
+++ src/ReverseNumber34.java
@@ -0,0 +1,20 @@
+//Program 34- WAP to reverse a given number.
+
+package src;
+
+import java.util.Scanner;
+
+public class ReverseNumber34 {
+    public static void main(String[] args) {
+        System.out.println("Enter the number: ");
+        int n = new Scanner(System.in).nextInt();
+
+        int digit = 0, rev = 0;
+        while (n != 0) {
+            digit = n % 10;
+            rev = (rev * 10) + digit;
+            n = n / 10;
+        }
+        System.out.println(rev);
+    }
+}
