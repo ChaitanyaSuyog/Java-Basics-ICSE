aaf8df7a1170cae583978a49ec414353651a72c9
Chaitanya S <chaitanya.suyog@gmail.com>
2021-10-05 12:19:29 +0530

    Program 36
    Print numbers 1 to 5 using do-while.

---
+++ src/DoWhile36.java
@@ -0,0 +1,13 @@
+//Program 36 - WAP to print numbers from 1 to 5 using a do-while loop.
+
+package src;
+
+public class DoWhile36 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }while(i <= 5);
+    }
+}
