024a5722dca114d7a2f899580bea173993e3c407
Chaitanya S <chaitanya.suyog@gmail.com>
2021-10-05 12:18:18 +0530

    .gitignore

--- Test.java
+++ Test.java
@@ -1,17 +1,9 @@
-import java.util.Scanner;
-
 class Test{
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the range (lower and higher numbers): ");
-        int num1 = scan.nextInt();
-        int num2 = scan.nextInt();
-
-        while (num1 < num2) {
-            num1++;
-            if (num1 % 2 == 0) {
-                System.out.println(num1);
-            }
-        }
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }while(i <= 5);
     }
 }
