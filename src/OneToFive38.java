efe1b6cf39b2271b51152a648066ea0e73e93a1a
Chaitanya S <chaitanya.suyog@gmail.com>
2021-10-19 12:06:14 +0530

    Program 38

---
+++ src/OneToFive38.java
@@ -0,0 +1,12 @@
+package src;
+
+public class OneToFive38 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
