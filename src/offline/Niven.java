3a1d893493d12698ec3e6c09ceeb34cc6d130f0a
Chaitanya S <chaitanya.suyog@gmail.com>
2022-02-13 05:55:24 +0530

    Completed Niven

--- src/offline/Niven.java
+++ src/offline/Niven.java
@@ -3,4 +3,26 @@ package src.offline;
 import java.util.Scanner;
 
 public class Niven {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number to check whether it is Niven or not: ");
+        int n = scan.nextInt();
+        int orgN = n;
+
+        int dig = 0, sum = 0;
+        while (n != 0) {
+            dig = n % 10;
+            sum = sum + dig;
+            n /= 10;
+        }
+
+        if (orgN % sum == 0) {
+            char d = '\u2223';
+            System.out.println(orgN + " is a Niven number (" + orgN + " " + d + " " + orgN + ")");
+        }
+        else {
+            char nd = '\u2224';
+            System.out.println(orgN + " is not a Niven number (" + orgN + " " + nd + " " + orgN + ")");
+        }
+    }
 }
