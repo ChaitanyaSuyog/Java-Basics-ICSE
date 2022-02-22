9f2add147e3a299b9e8a4da74937bb4ef1afea9d
Chaitanya S <chaitanya.suyog@gmail.com>
2022-02-22 22:06:52 +0530

    Tests

--- Test.java
+++ Test.java
@@ -1,9 +1,17 @@
-class Test{
+import java.util.Scanner;
+
+class Test {
     public static void main(String[] args) {
-        int i = 1;
-        do {
-            System.out.println(i);
-            i++;
-        }while(i <= 5);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the value of x and the number of terms:");
+        double x = scan.nextDouble();
+        int n = scan.nextInt();
+        double dr = 2d;
+        double sum = 0d;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + (x/dr);
+            dr += 2;
+        }
+        System.out.println(sum);
     }
 }
