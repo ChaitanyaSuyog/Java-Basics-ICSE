6eb3de7eed51cf1e92daef8bb8f729a5736046aa
Chaitanya S <chaitanya.suyog@gmail.com>
2022-10-14 22:25:13 +0530

    TechNo Program
    Came for TA-1

---
+++ src/offline/TechNo.java
@@ -0,0 +1,21 @@
+package src.offline;
+
+public class TechNo {
+    public static void checkNo(int n) {
+        int oN = n;
+        int half1 = n % 100;
+        n = n / 100;
+        int half2 = n;
+
+        if (Math.pow(half1 + half2, 2) == oN) {
+            System.out.println(oN);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1000; i <= 9999; i++) {
+            checkNo(i);
+        }
+        System.out.println("The above no.s are all 4-digit tech no.s");
+    }
+}
